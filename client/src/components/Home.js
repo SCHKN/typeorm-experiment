c67daed44eb4bb726c69d0de865dd3e6849c8c81
SCHKN <solnichkin.antoine@gmail.com>
2018-03-11 16:06:43 +0100

    built a cool form for signup

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Search } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 
 class Home extends React.Component {
   render() {
--- client/src/components/auth/SignUp.js
+++ client/src/components/auth/SignUp.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Button, Form, Header, Segment, Grid } from 'semantic-ui-react'
 
 class SignUp extends React.Component {
   render() {
     return (
-      <div>
+      <div className="signup-form">
+        <Grid
+          verticalAlign='middle'
+          style={{ height: '100%' }}
+          textAlign='center'
+          >
+          <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as='h2' color='violet' textAlign='center'>
+            {' '}Sign Up
+            </Header>
+          <Form>
+            <Segment raised>
+            <Form.Field>
+              <input placeholder='Email' />
+            </Form.Field>
+            <Form.Field>
+              <input placeholder='Name you will use on this website' />
+            </Form.Field>
+            <Form.Field>
+              <input placeholder='Password' type='password'/>
+            </Form.Field>
+            <Button type='submit' color='violet'>Submit</Button>
+            </Segment>
+          </Form>
+          </Grid.Column>
+        </Grid>
       </div>
     )
   }
